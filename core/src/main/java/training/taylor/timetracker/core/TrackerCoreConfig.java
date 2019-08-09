6b2d974490972bb413958f8955601d62e0d71b77
elife2000 <elife2000@gmail.com>
2019-08-09 06:18:52 +0000

    commit

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -1,6 +1,6 @@
 package training.taylor.timetracker.core;
 
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import training.taylor.timetracker.core.dao.TimeEntry;
@@ -15,8 +15,8 @@ import java.util.List;
 @ComponentScan("training.taylor.timetracker.core")
 public class TrackerCoreConfig {
 
-//    @Bean(name = "timesheet")
-    public List<TimeEntry> timeEntries() {
-        return new ArrayList<>();
-    }
+//    @Bean(name = "timesheet"
+//    public List<TimeEntry> timeEntries() {
+//        return new ArrayList<>();
+//    }
 }
